512b58386c00c91d77cad71850c74a988c1930e6
delindell <daviselindell@gmail.com>
2020-02-29 11:34:05 -0600

    can kill people

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,19 @@
 import tank from "./components/shark-tank.js"
 import graveyard from "./components/graveyard.js"
+import personData from "./helpers/data/personData.js"
+
+const sharkAttackEvent = () => {
+    console.log('ouch')
+    personData.randomMurder();
+    graveyard.buildGraveyard()
+    tank.tankBuilder()
+}
 
 const init = () => {
     graveyard.buildGraveyard()
     tank.tankBuilder()
+    $('#bite-me').click(sharkAttackEvent)
 }
+
 
 init()
